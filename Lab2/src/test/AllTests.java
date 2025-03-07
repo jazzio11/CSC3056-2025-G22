18fa804c1ca7221ce0c002232150404e1ebd46a9
jazzio11 <95865137+jazzio11@users.noreply.github.com>
2025-03-07 10:11:37 +0000

    java project
    java project

---
+++ Lab2/src/test/AllTests.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.jfree.data.Values2D;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class AllTests {
+	@testc
+	public void test() {
+		
+	Values2D data; 
+		
+	}
+
+}
